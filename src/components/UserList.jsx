bbef3e803989785f275d39db297150c05f7f20d3
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-01-30 18:09:15 -0500

    Add basic fetch for user list

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,16 +1,58 @@
-import React, { useState } from "react";
-// import apiSource
+import React, { useState, useEffect } from "react";
+import { apiurl } from "../apiSource";
 
-
-function UserList({
+function UserList(
+  {
     // Props
-}) {
-    // State declarations
-    // Functions
-    // Render
-    return (
-        <div>User List</div>
-    )
+  }
+) {
+  // State declarations
+  const [userList, setUserList] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Functions
+  useEffect(() => {
+    fetch(apiurl + "user", {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("User list fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setUserList(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+  // Render
+  if (loading) return <p>Loading user list...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
+
+  return (
+    <div>
+      <p>User List</p>
+      <p>To-do: Search Bar</p>
+      {userList.length == 0 ? (
+        <p>No users found</p>
+      ) : (
+        <ul>
+          {userList.map((user) => {
+            return (
+              <li>
+                <p>{user.username}</p>
+                {/* Link to details, edit form, delete option */}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default UserList
+export default UserList;
