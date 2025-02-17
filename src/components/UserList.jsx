46ccae8d2b52b8477f750d2ea5e12c47bfa6d98c
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-02-17 17:15:05 -0500

    Add search form to user list view

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -8,6 +8,7 @@ function UserList(
   }
 ) {
   // State declarations
+  const [query, setQuery] = useState("");
   const [userList, setUserList] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -30,6 +31,28 @@ function UserList(
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
   }, []);
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
+  async function submitQuery(e) {
+    e.preventDefault();
+    await fetch(apiurl + `user/?username=${query}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("User list fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setUserList(response));
+  }
+
   // Render
   if (loading) return <p>Loading user list...</p>;
   if (error) return <p>Network error, please try again later.</p>;
@@ -38,9 +61,16 @@ function UserList(
     <div className="mainDiv">
       <Link to={"/"}>Back</Link>
       <h1 className="pageHeader">User List</h1>
-      <form className="blueBlock">
-        {" "}
-        <p>To-do: Search Bar</p>
+      <form onSubmit={submitQuery} className="blueBlock">
+        <label htmlFor="queryTag">Search Users:</label>
+        <input
+          type="text"
+          name="queryTag"
+          id="queryTag"
+          value={query}
+          onChange={handleQuery}
+        />
+        <button>Search</button>
       </form>
       {userList.length == 0 ? (
         <p>No users found</p>
